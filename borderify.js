b6052b4681701a42ba2dacc8b19b84717cc9438c
sdlyyxy <sdlyyxy@icloud.com>
2022-03-03 20:15:23 +0800

    Init

---
+++ borderify.js
@@ -0,0 +1,6 @@
+// document.body.style.border = "5px solid red";
+document.querySelector(".js-consent-banner").remove();
+// document.querySelector("#noscript-warning").remove();
+document.querySelector(".z-nav").remove();
+
+// js-consent-banner
